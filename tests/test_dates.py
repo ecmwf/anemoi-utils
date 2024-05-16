5569fc7e29d70980f0f7acd2dbd16a13fda4126d
Florian Pinault <Florian.Pinault@ecmwf.int>
2024-05-16 07:57:38 +0000

    simplify tests

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -8,17 +8,15 @@ from anemoi.utils.dates import datetimes_factory
 def _(txt):
     txt = dedent(txt)
     config = yaml.safe_load(txt)
-    config = config["dates"]
     return datetimes_factory(config)
 
 
 def test_date_1():
     d = _(
         """
-        dates:
-            - 2023-01-01
-            - 2023-01-02
-            - 2023-01-03
+          - 2023-01-01
+          - 2023-01-02
+          - 2023-01-03
     """
     )
     assert len(list(d)) == 3
@@ -27,11 +25,10 @@ def test_date_1():
 def test_date_2():
     d = _(
         """
-        dates:
-          start: 2023-01-01
-          end: 2023-01-07
-          frequency: 12
-          day_of_week: [monday, friday]
+        start: 2023-01-01
+        end: 2023-01-07
+        frequency: 12
+        day_of_week: [monday, friday]
     """
     )
     assert len(list(d)) == 4
@@ -40,13 +37,12 @@ def test_date_2():
 def test_date_3():
     d = _(
         """
-        dates:
-          - start: 2023-01-01
-            end: 2023-01-03
-            frequency: 24
-          - start: 2024-01-01T06:00:00
-            end: 2024-01-03T18:00:00
-            frequency: 6
+        - start: 2023-01-01
+          end: 2023-01-03
+          frequency: 24
+        - start: 2024-01-01T06:00:00
+          end: 2024-01-03T18:00:00
+          frequency: 6
     """
     )
     assert len(list(d)) == 14
@@ -55,13 +51,12 @@ def test_date_3():
 def test_date_hindcast_1():
     d = _(
         """
-        dates:
-          - name: hindcast
-            reference_dates:
-              start: 2023-01-01
-              end: 2023-01-03
-              frequency: 24
-            years: 20
+        - name: hindcast
+          reference_dates:
+            start: 2023-01-01
+            end: 2023-01-03
+            frequency: 24
+          years: 20
     """
     )
     assert len(list(d)) == 60
@@ -70,13 +65,12 @@ def test_date_hindcast_1():
 def test_date_hindcast_2():
     d = _(
         """
-        dates:
-          - name: hindcast
-            reference_dates:
-              start: 2023-01-01
-              end: 2023-01-03
-              frequency: 24
-            years: [2018, 2019, 2020, 2021]
+        - name: hindcast
+          reference_dates:
+            start: 2023-01-01
+            end: 2023-01-03
+            frequency: 24
+          years: [2018, 2019, 2020, 2021]
     """
     )
     assert len(list(d)) == 12
