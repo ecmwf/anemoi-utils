8fe64c9d16234fc7022fe24c1ce6a463ea22ba9a
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-05-25 15:41:34 +0000

    Add timer

--- src/anemoi/utils/commands/checkpoint.py
+++ src/anemoi/utils/commands/checkpoint.py
@@ -14,15 +14,47 @@ import json
 from . import Command
 
 
+def visit(x, path, name, value):
+    if isinstance(x, dict):
+        for k, v in x.items():
+            if k == name:
+                print(".".join(path), k, v)
+
+            if v == value:
+                print(".".join(path), k, v)
+
+            path.append(k)
+            visit(v, path, name, value)
+            path.pop()
+
+    if isinstance(x, list):
+        for i, v in enumerate(x):
+            path.append(str(i))
+            visit(v, path, name, value)
+            path.pop()
+
+
 class Checkpoint(Command):
 
     def add_arguments(self, command_parser):
         command_parser.add_argument("path", help="Path to the checkpoint.")
+        command_parser.add_argument("--name", help="Search for a specific name.")
+        command_parser.add_argument("--value", help="Search for a specific value.")
 
     def run(self, args):
         from anemoi.utils.checkpoints import load_metadata
 
         checkpoint = load_metadata(args.path, "*.json")
+
+        if args.name or args.value:
+            visit(
+                checkpoint,
+                [],
+                args.name if args.name is not None else object(),
+                args.value if args.value is not None else object(),
+            )
+            return
+
         print(json.dumps(checkpoint, sort_keys=True, indent=4))
 
 
---
+++ src/anemoi/utils/timer.py
@@ -0,0 +1,32 @@
+# (C) Copyright 2024 European Centre for Medium-Range Weather Forecasts.
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+"""Logging utilities."""
+
+import logging
+import time
+
+from .humanize import seconds
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Timer:
+    def __init__(self, title, logger=LOGGER):
+        self.title = title
+        self.start = time.time()
+        self.logger = logger
+
+    def __enter__(self):
+        return self
+
+    @property
+    def elapsed(self):
+        return time.time() - self.start
+
+    def __exit__(self, *args):
+        self.logger.info("%s: %s.", self.title, seconds(self.elapsed))
