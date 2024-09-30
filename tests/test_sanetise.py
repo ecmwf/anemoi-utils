4f59e3517e4aa7480feb7107850d76a698f58b4c
Florian Pinault <floriankrb@users.noreply.github.com>
2024-09-30 13:18:28 +0200

    fix typo (#11)
    * fix typo

--- tests/test_sanetise.py
+++ tests/test_sanetise.py
@@ -6,60 +6,60 @@
 # nor does it submit to any jurisdiction.
 
 
-from anemoi.utils.sanetise import sanetise
+from anemoi.utils.sanitise import sanitise
 
 
-def test_sanetise_urls():
-    assert sanetise("http://johndoe:password@host:port/path") == "http://user:***@host:port/path"
+def test_sanitise_urls():
+    assert sanitise("http://johndoe:password@host:port/path") == "http://user:***@host:port/path"
 
-    assert sanetise("http://www.example.com/path?pass=secret") == "http://www.example.com/path?pass=hidden"
-    assert sanetise("http://www.example.com/path?password=secret") == "http://www.example.com/path?password=hidden"
-    assert sanetise("http://www.example.com/path?token=secret") == "http://www.example.com/path?token=hidden"
-    assert sanetise("http://www.example.com/path?user=secret") == "http://www.example.com/path?user=hidden"
-    assert sanetise("http://www.example.com/path?key=secret") == "http://www.example.com/path?key=hidden"
-    assert sanetise("http://www.example.com/path?pwd=secret") == "http://www.example.com/path?pwd=hidden"
-    assert sanetise("http://www.example.com/path?_key=secret") == "http://www.example.com/path?_key=hidden"
-    assert sanetise("http://www.example.com/path?_token=secret") == "http://www.example.com/path?_token=hidden"
-    assert sanetise("http://www.example.com/path?apikey=secret") == "http://www.example.com/path?apikey=hidden"
-    assert sanetise("http://www.example.com/path?api_key=secret") == "http://www.example.com/path?api_key=hidden"
-    assert sanetise("http://www.example.com/path?api_token=secret") == "http://www.example.com/path?api_token=hidden"
-    assert sanetise("http://www.example.com/path?_api_token=secret") == "http://www.example.com/path?_api_token=hidden"
-    assert sanetise("http://www.example.com/path?_api_key=secret") == "http://www.example.com/path?_api_key=hidden"
-    assert sanetise("http://www.example.com/path?username=secret") == "http://www.example.com/path?username=hidden"
-    assert sanetise("http://www.example.com/path?login=secret") == "http://www.example.com/path?login=hidden"
+    assert sanitise("http://www.example.com/path?pass=secret") == "http://www.example.com/path?pass=hidden"
+    assert sanitise("http://www.example.com/path?password=secret") == "http://www.example.com/path?password=hidden"
+    assert sanitise("http://www.example.com/path?token=secret") == "http://www.example.com/path?token=hidden"
+    assert sanitise("http://www.example.com/path?user=secret") == "http://www.example.com/path?user=hidden"
+    assert sanitise("http://www.example.com/path?key=secret") == "http://www.example.com/path?key=hidden"
+    assert sanitise("http://www.example.com/path?pwd=secret") == "http://www.example.com/path?pwd=hidden"
+    assert sanitise("http://www.example.com/path?_key=secret") == "http://www.example.com/path?_key=hidden"
+    assert sanitise("http://www.example.com/path?_token=secret") == "http://www.example.com/path?_token=hidden"
+    assert sanitise("http://www.example.com/path?apikey=secret") == "http://www.example.com/path?apikey=hidden"
+    assert sanitise("http://www.example.com/path?api_key=secret") == "http://www.example.com/path?api_key=hidden"
+    assert sanitise("http://www.example.com/path?api_token=secret") == "http://www.example.com/path?api_token=hidden"
+    assert sanitise("http://www.example.com/path?_api_token=secret") == "http://www.example.com/path?_api_token=hidden"
+    assert sanitise("http://www.example.com/path?_api_key=secret") == "http://www.example.com/path?_api_key=hidden"
+    assert sanitise("http://www.example.com/path?username=secret") == "http://www.example.com/path?username=hidden"
+    assert sanitise("http://www.example.com/path?login=secret") == "http://www.example.com/path?login=hidden"
 
-    assert sanetise("http://www.example.com/path;pass=secret") == "http://www.example.com/path;pass=hidden"
-    assert sanetise("http://www.example.com/path;password=secret") == "http://www.example.com/path;password=hidden"
-    assert sanetise("http://www.example.com/path;token=secret") == "http://www.example.com/path;token=hidden"
-    assert sanetise("http://www.example.com/path;user=secret") == "http://www.example.com/path;user=hidden"
-    assert sanetise("http://www.example.com/path;key=secret") == "http://www.example.com/path;key=hidden"
-    assert sanetise("http://www.example.com/path;pwd=secret") == "http://www.example.com/path;pwd=hidden"
-    assert sanetise("http://www.example.com/path;_key=secret") == "http://www.example.com/path;_key=hidden"
-    assert sanetise("http://www.example.com/path;_token=secret") == "http://www.example.com/path;_token=hidden"
-    assert sanetise("http://www.example.com/path;apikey=secret") == "http://www.example.com/path;apikey=hidden"
-    assert sanetise("http://www.example.com/path;api_key=secret") == "http://www.example.com/path;api_key=hidden"
-    assert sanetise("http://www.example.com/path;api_token=secret") == "http://www.example.com/path;api_token=hidden"
-    assert sanetise("http://www.example.com/path;_api_token=secret") == "http://www.example.com/path;_api_token=hidden"
-    assert sanetise("http://www.example.com/path;_api_key=secret") == "http://www.example.com/path;_api_key=hidden"
-    assert sanetise("http://www.example.com/path;username=secret") == "http://www.example.com/path;username=hidden"
-    assert sanetise("http://www.example.com/path;login=secret") == "http://www.example.com/path;login=hidden"
+    assert sanitise("http://www.example.com/path;pass=secret") == "http://www.example.com/path;pass=hidden"
+    assert sanitise("http://www.example.com/path;password=secret") == "http://www.example.com/path;password=hidden"
+    assert sanitise("http://www.example.com/path;token=secret") == "http://www.example.com/path;token=hidden"
+    assert sanitise("http://www.example.com/path;user=secret") == "http://www.example.com/path;user=hidden"
+    assert sanitise("http://www.example.com/path;key=secret") == "http://www.example.com/path;key=hidden"
+    assert sanitise("http://www.example.com/path;pwd=secret") == "http://www.example.com/path;pwd=hidden"
+    assert sanitise("http://www.example.com/path;_key=secret") == "http://www.example.com/path;_key=hidden"
+    assert sanitise("http://www.example.com/path;_token=secret") == "http://www.example.com/path;_token=hidden"
+    assert sanitise("http://www.example.com/path;apikey=secret") == "http://www.example.com/path;apikey=hidden"
+    assert sanitise("http://www.example.com/path;api_key=secret") == "http://www.example.com/path;api_key=hidden"
+    assert sanitise("http://www.example.com/path;api_token=secret") == "http://www.example.com/path;api_token=hidden"
+    assert sanitise("http://www.example.com/path;_api_token=secret") == "http://www.example.com/path;_api_token=hidden"
+    assert sanitise("http://www.example.com/path;_api_key=secret") == "http://www.example.com/path;_api_key=hidden"
+    assert sanitise("http://www.example.com/path;username=secret") == "http://www.example.com/path;username=hidden"
+    assert sanitise("http://www.example.com/path;login=secret") == "http://www.example.com/path;login=hidden"
 
 
-def test_sanetise_paths():
+def test_sanitise_paths():
     # We want to keep earthkit-data's url and path pattern
 
-    assert sanetise("/home/johndoe/.ssh/id_rsa") == "/.../id_rsa"
+    assert sanitise("/home/johndoe/.ssh/id_rsa") == "/.../id_rsa"
 
     assert (
-        sanetise("/data/model/{date:strftime(%Y)}/{date:strftime(%m)}/{date:strftime(%d)}/analysis.grib")
+        sanitise("/data/model/{date:strftime(%Y)}/{date:strftime(%m)}/{date:strftime(%d)}/analysis.grib")
         == "/.../{date:strftime(%Y)}/{date:strftime(%m)}/{date:strftime(%d)}/analysis.grib"
     )
 
-    assert sanetise("test.grib") == "test.grib"
-    assert sanetise("../test.grib") == "../test.grib"
-    assert sanetise("./test.grib") == "./test.grib"
-    assert sanetise("sub/folder/test.grib") == "sub/folder/test.grib"
-    assert sanetise("./folder/test.grib") == "./folder/test.grib"
+    assert sanitise("test.grib") == "test.grib"
+    assert sanitise("../test.grib") == "../test.grib"
+    assert sanitise("./test.grib") == "./test.grib"
+    assert sanitise("sub/folder/test.grib") == "sub/folder/test.grib"
+    assert sanitise("./folder/test.grib") == "./folder/test.grib"
 
 
 if __name__ == "__main__":
