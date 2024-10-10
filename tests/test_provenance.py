8c64b0bdad3c582c05d4db2fd2fb870b87ce8bb4
Harrison Cook <harrison.cook@ecmwf.int>
2024-10-10 08:06:47 +0000

    Add test

---
+++ tests/test_provenance.py
@@ -0,0 +1,14 @@
+# (C) Copyright 2024 European Centre for Medium-Range Weather Forecasts.
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+from anemoi.utils import provenance
+
+
+def test_gather():
+    """Test success of gather_provenance_info"""
+    provenance.gather_provenance_info()
