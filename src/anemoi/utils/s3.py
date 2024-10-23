2b59bb540ebc693f3531780d982d47aa56bdd5ae
b8raoult <53792887+b8raoult@users.noreply.github.com>
2024-10-23 12:13:44 +0100

    Update copyright notice

--- src/anemoi/utils/s3.py
+++ src/anemoi/utils/s3.py
@@ -1,9 +1,12 @@
-# (C) Copyright 2024 European Centre for Medium-Range Weather Forecasts.
+# (C) Copyright 2024 Anemoi contributors.
+#
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
 # In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
+
 
 """This module provides functions to upload, download, list and delete files and folders on S3.
 The functions of this package expect that the AWS credentials are set up in the environment
