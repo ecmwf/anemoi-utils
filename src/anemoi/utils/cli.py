e05b13258975b0f5b307a9a10d799edff86033c8
Gert Mertes <gert.mertes@ecmwf.int>
2024-10-31 11:21:31 +0000

    refactor: rename `name` to `command`

--- src/anemoi/utils/cli.py
+++ src/anemoi/utils/cli.py
@@ -97,8 +97,8 @@ def register_commands(here, package, select, fail=None):
 
         obj = select(imported)
         if obj is not None:
-            if hasattr(obj, "name"):
-                name = obj.name
+            if hasattr(obj, "command"):
+                name = obj.command
             result[name] = obj
 
     for name, e in not_available.items():
