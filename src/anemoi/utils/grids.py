c06cd5d58a5cfd5f7c7992867d29077f6f41e828
Florian Pinault <floriankrb@users.noreply.github.com>
2025-01-14 21:27:17 +0100

    feat: added-anemoi-utils-grids-and-tests (#74)
    * feat: added-anemoi-utils-grids-and-tests
    
    * added numpy as a dependency
    
    * adding custom path for grids in the .config/anemoi/settings.toml [utils] grid_paths = "/some/path/to/my/npz"
    
    * fix

---
+++ src/anemoi/utils/grids.py
@@ -0,0 +1,54 @@
+# (C) Copyright 2025 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+"""Utilities for working with grids.
+
+"""
+
+import logging
+import os
+from io import BytesIO
+
+import numpy as np
+import requests
+
+from .caching import cached
+
+LOG = logging.getLogger(__name__)
+
+
+GRIDS_URL_PATTERN = "https://get.ecmwf.int/repository/anemoi/grids/grid-{name}.npz"
+
+
+@cached(collection="grids", encoding="npz")
+def _grids(name):
+    from anemoi.utils.config import load_config
+
+    user_path = load_config().get("utils", {}).get("grids_path")
+    if user_path:
+        path = os.path.expanduser(os.path.join(user_path, f"grid-{name}.npz"))
+        if os.path.exists(path):
+            LOG.warning("Loading grids from custom user path %s", path)
+            with open(path, "rb") as f:
+                return f.read()
+        else:
+            LOG.warning("Custom user path %s does not exist", path)
+
+    url = GRIDS_URL_PATTERN.format(name=name.lower())
+    LOG.warning("Downloading grids from %s", url)
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content
+
+
+def grids(name):
+    data = _grids(name)
+    npz = np.load(BytesIO(data))
+    return dict(npz)
---
+++ tests/test_grids.py
@@ -0,0 +1,28 @@
+# (C) Copyright 2025 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+from anemoi.utils.grids import grids
+
+
+def test_o96():
+    x = grids("o96")
+    assert x["latitudes"].mean() == 0.0
+    assert x["longitudes"].mean() == 179.14285714285714
+    assert x["latitudes"].shape == (40320,)
+    assert x["longitudes"].shape == (40320,)
+    assert x["latitudes"][31415] == -31.324557701757268
+    assert x["longitudes"][31415] == 224.32835820895522
+
+
+if __name__ == "__main__":
+    for name, obj in list(globals().items()):
+        if name.startswith("test_") and callable(obj):
+            print(f"Running {name}...")
+            obj()
