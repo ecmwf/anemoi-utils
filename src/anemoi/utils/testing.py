5b17cbad749987a19482ee69ccebe2ea465b1c05
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-03-15 11:30:50 +0000

    testing

--- src/anemoi/utils/testing.py
+++ src/anemoi/utils/testing.py
@@ -24,8 +24,14 @@ lock = threading.Lock()
 TEMPORARY_DIRECTORY = None
 
 
-def temporary_directory() -> str:
-    """Return a temporary in which to download test data."""
+def _temporary_directory() -> str:
+    """Return a temporary directory in which to download test data.
+
+    Returns
+    -------
+    str
+        The path to the temporary directory.
+    """
     global TEMPORARY_DIRECTORY
     with lock:
         if TEMPORARY_DIRECTORY is not None:
@@ -39,18 +45,64 @@ def temporary_directory() -> str:
         return TEMPORARY_DIRECTORY
 
 
-def get_test_data(path: str) -> str:
+def _check_path(path: str) -> None:
+    """Check if the given path is normalized, not absolute, and does not start with a dot.
+
+    Parameters
+    ----------
+    path : str
+        The path to check.
+
+    Raises
+    ------
+    AssertionError
+        If the path is not normalized, is absolute, or starts with a dot.
+    """
     assert os.path.normpath(path) == path, f"Path '{path}' should be normalized"
     assert not os.path.isabs(path), f"Path '{path}' should not be absolute"
     assert not path.startswith("."), f"Path '{path}' should not start with '.'"
 
-    target = os.path.normpath(os.path.join(temporary_directory(), path))
+
+def url_for_test_data(path: str) -> str:
+    """Generate the URL for the test data based on the given path.
+
+    Parameters
+    ----------
+    path : str
+        The relative path to the test data.
+
+    Returns
+    -------
+    str
+        The full URL to the test data.
+    """
+    _check_path(path)
+
+    return f"{TEST_DATA_URL}{path}"
+
+
+def get_test_data(path: str) -> str:
+    """Download the test data to a temporary directory and return the local path.
+
+    Parameters
+    ----------
+    path : str
+        The relative path to the test data.
+
+    Returns
+    -------
+    str
+        The local path to the downloaded test data.
+    """
+    _check_path(path)
+
+    target = os.path.normpath(os.path.join(_temporary_directory(), path))
     with lock:
         if os.path.exists(target):
             return target
 
         os.makedirs(os.path.dirname(target), exist_ok=True)
-        url = f"{TEST_DATA_URL}{path}"
+        url = url_for_test_data(path)
         LOG.info(f"Downloading test data from {url} to {target}")
 
         download(url, target)
