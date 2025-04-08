dd7e034e053330b217cc567d431de0ec29b82224
Vera Gahlen <vera.gahlen@ecmwf.int>
2025-04-08 11:58:40 +0000

    remove archive after unpacking

--- src/anemoi/utils/testing.py
+++ src/anemoi/utils/testing.py
@@ -157,6 +157,8 @@ def get_test_archive(path: str, extension=".extracted") -> str:
         shutil.unpack_archive(archive, os.path.dirname(target) + ".tmp")
         os.rename(os.path.dirname(target) + ".tmp", target)
 
+        os.remove(archive)
+
         return target
 
 
