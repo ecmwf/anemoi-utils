b479b1163d8b864572cd31b8528d8b68ba93534b
Gareth Jones <gareth.jones@ecmwf.int>
2025-06-18 16:50:36 +0000

    Add metadata removal command, update transfer help text

---
+++ src/anemoi/utils/commands/delete-metadata.py
@@ -0,0 +1,45 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+from argparse import ArgumentParser
+from argparse import Namespace
+
+from anemoi.utils.checkpoints import remove_metadata
+
+from . import Command
+
+
+class DeleteMetadata(Command):
+    """Delete the metadata from a checkpoint, this commands updates the file in place."""
+
+    def add_arguments(self, command_parser: ArgumentParser) -> None:
+        """Add arguments to the command parser.
+
+        Parameters
+        ----------
+        command_parser : ArgumentParser
+            The argument parser to which the arguments will be added.
+        """
+        command_parser.add_argument(
+            "filename", help="A path to the checkpoint file containing the metadata."
+        )
+    
+    def run(self, args: Namespace) -> None:
+        """Execute the command with the provided arguments.
+
+        Parameters
+        ----------
+        args : Namespace
+            The arguments passed to the command.
+        """
+        remove_metadata(args.filename)
+
+
+command = DeleteMetadata
