2a25fb2e607af6617c19725d4c23f157d3f64ef2
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-09-20 10:18:07 +0100

    feat: add aliases to registry

--- src/anemoi/utils/registry.py
+++ src/anemoi/utils/registry.py
@@ -97,6 +97,8 @@ class Registry(Generic[T]):
         self.package = package
         self.__registered = {}
         self._sources = {}
+        self._aliases = {}
+        self._warnings = set()
         self.kind = package.split(".")[-1]
         self.key = key
         self.api_version = api_version
@@ -118,7 +120,9 @@ class Registry(Generic[T]):
         """
         return _BY_KIND.get(kind)
 
-    def register(self, name: str, factory: Callable | None = None, source: Any | None = None) -> Wrapper | None:
+    def register(
+        self, name: str, factory: Callable | None = None, source: Any | None = None, aliases: list[str] | None = None
+    ) -> Wrapper | None:
         """Register a factory with the registry.
 
         Parameters
@@ -129,6 +133,8 @@ class Registry(Generic[T]):
             The factory to register, by default None.
         source : Any, optional
             The source of the factory, by default None.
+        aliases : list of str, optional
+            Aliases for the factory, by default None.
 
         Returns
         -------
@@ -136,7 +142,13 @@ class Registry(Generic[T]):
             A wrapper if the factory is None, otherwise None.
         """
 
+        aliases = aliases or []
+
         name = name.replace("_", "-")
+        assert (
+            name not in self._aliases
+        ), f"'{name}' is already registered for '{self._aliases[name]}' in {self.package}"
+        assert name not in aliases, f"'{name}' cannot be an alias for itself in {self.package}"
 
         if factory is None:
             # This happens when the @register decorator is used
@@ -149,6 +161,15 @@ class Registry(Generic[T]):
             warnings.warn(f"Factory '{name}' is already registered in {self.package}")
             warnings.warn(f"Existing: {self._sources[name]}")
             warnings.warn(f"New: {source}")
+
+        for alias in aliases:
+            assert (
+                alias not in self.__registered
+            ), f"Alias '{alias}' is already registered as a factory in {self.package}"
+            alias = alias.replace("_", "-")
+            if alias in self._aliases:
+                warnings.warn(f"Alias '{alias}' is already registered for '{self._aliases[alias]}' in {self.package}")
+            self._aliases[alias] = name
 
         self.__registered[name] = factory
         self._sources[name] = source
@@ -188,6 +209,7 @@ class Registry(Generic[T]):
         """
 
         name = name.replace("_", "-")
+        name = self.unalias(name)
 
         ok = name in self.factories
         if not ok:
@@ -213,6 +235,7 @@ class Registry(Generic[T]):
         """
 
         name = name.replace("_", "-")
+        name = self.unalias(name)
 
         if return_none:
             return self.factories.get(name)
@@ -305,6 +328,7 @@ class Registry(Generic[T]):
         """
 
         name = name.replace("_", "-")
+        name = self.unalias(name)
 
         factory = self.lookup(name)
         return factory(*args, **kwargs)
@@ -352,3 +376,26 @@ class Registry(Generic[T]):
         raise ValueError(
             f"Entry '{config}' must either be a string, a dictionary with a single entry, or a dictionary with a '{self.key}' key"
         )
+
+    def unalias(self, name: str) -> str:
+        """Resolve an alias to its canonical name.
+
+        Parameters
+        ----------
+        name : str
+            The name to resolve.
+
+        Returns
+        -------
+        str
+            The canonical name.
+        """
+        canonical = self._aliases.get(name, name)
+        if canonical != name:
+            warnings.warn(
+                f"Alias '{name}' for '{canonical}' in {self.package} is deprecated and will be removed in a future version.",
+                category=DeprecationWarning,
+                # stacklevel=2,
+            )
+
+        return canonical
